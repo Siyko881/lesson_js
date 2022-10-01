379244fc2f03354ecd85a713ed219966cb9b14e0
Serhiy <kuznjakserhij@gmail.com>
2022-10-01 15:57:13 +0300

    firstcommit

---
+++ js/lesson_ obj_arr.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// let storeName = "rozetka";
+// const storeDescription = {
+//     budget: 10000,
+//     employees: ["Petro", "Ivan", "Oleg"],
+//     products: {iphone: 100000, tv: 5000},
+//     open: true
+// };
+// console.log(storeName, storeDescription.budget);
+
+// const answer = +prompt("Скільки вам років?","");
+// console.log(answer);
+// if (answer >=18 ){
+//     alert("True");
+// } else {
+//     alert("False");
+// }
+
+const infoUser = [];
+
+infoUser [0] = prompt("Як вас звати?","");
+infoUser [1] = prompt("Скільки вам років?","");
+infoUser [2] = prompt("Ваше місце проживання?","");
+document.write(infoUser);
